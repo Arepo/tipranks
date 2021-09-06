384b5e14b6d334af48335179ff1dc5c137c129d0
Arepo <jinksy@gmail.com>
2021-09-06 23:34:42 +0100

    ... and Wall St scraper still working

--- scraper_test.py
+++ scraper_test.py
@@ -6,6 +6,6 @@ def test_retail_scraper():
     scraper = AnalystScraper(RetailConfig(number_of_analysts=4))
     assert scraper.count_recommendations()['Novavax'] == 1
 
-# def test_wall_st_scraper():
-#     scraper = AnalystScraper(WallStConfig(number_of_analysts=1))
-#     assert scraper.count_recommendations()['DocuSign'] == 1
+def test_wall_st_scraper():
+    scraper = AnalystScraper(WallStConfig(number_of_analysts=1))
+    assert scraper.count_recommendations()['DocuSign'] == 1
